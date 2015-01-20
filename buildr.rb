a6dd888eefb45a220f661d5a2fe0ab08f96d226d
john layton <johnstewartlayton@gmail.com>
2015-01-21 10:33:00 +1100

    update to force upgrade

--- buildr.rb
+++ buildr.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Buildr < Formula
   homepage ''
-  url 'https://github.com/johnlayton/anamastreet/raw/master/dist/buildr-0.0.3.tar.gz'
-  sha1 'ab19522087f2fa2198b890e4e71551cd60425937'
+  url 'https://github.com/johnlayton/anamastreet/raw/master/dist/buildr-0.0.4.tar.gz'
+  sha1 '881a9b23cf85ed5ae6e9fdb0c56ed44a97e1f829'
 
   def install
     if build.with? "completions"
--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,22 @@ var path = require( 'path' );
 var spawn = require( 'child_process' ).spawn;
 
 var version = require( './package.json' ).version;
-var cwd = path.resolve( process.cwd(), '..' );
-
-console.log( cwd )
+//var cwd = path.resolve( process.cwd(), '..' );
+//
+//console.log( cwd )
 
 gulp.task( 'default', function () {
   tar = spawn( 'tar', [
     '-cpvzf',
-    'anamastreet/dist/buildr-' + version + '.tar.gz',
-    'anamastreet/completions'
-  ], { cwd: cwd } );
+    'dist/buildr-' + version + '.tar.gz',
+    'buildr'
+  ] );
   tar.on( 'close', function ( code ) {
+
     var version = require( './package.json' ).version;
     var filename = './dist/buildr-' + version + '.tar.gz';
     var contents = require( 'fs' ).readFileSync( filename );
+
     gulp.src( './tmpl/buildr.rb' )
       .pipe( tmpl( {
                      version : version,
