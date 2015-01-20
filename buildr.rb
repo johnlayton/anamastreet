7c22fc476fa10f520624dcb5feb1a1ae2cf1a81b
john layton <johnstewartlayton@gmail.com>
2015-01-21 08:59:53 +1100

    update to force upgrade

--- buildr.rb
+++ buildr.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Buildr < Formula
   homepage ''
-  url 'https://github.com/johnlayton/anamastreet/raw/master/dist/buildr-0.0.1.tar.gz'
-  sha1 'fa08ac78adc1e7ad374651c1f2b00ad33a46631f'
+  url 'https://github.com/johnlayton/anamastreet/raw/master/dist/buildr-0.0.2.tar.gz'
+  sha1 'cac29ac2af5b050d62b30c132bbb9b0c1876571d'
 
   def install
     bash_completion.install "completions/buildr.bash"
